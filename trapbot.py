e51bb02e623ecca9cd6630d6dc8e76d2d12ebfd8
Kevin Mok <kevin.mok@live.ca>
2018-02-19 19:52:20 -0500

    Return link to search of song when bot can't find
    Also resolved #6.

--- trapbot.py
+++ trapbot.py
@@ -18,7 +18,6 @@ from googleapiclient.discovery import build
 #  }}} imports #
 
 #  global vars {{{ # 
-#  client = soundcloud.Client(client_id="BLIKpIMvNL9As25CHX4l9xXLu7KuU5uF")
 # Location of file where id's of already visited comments are maintained
 comments_path = '~/Documents/coding/trapbot/commented.txt'
 api_file = 'google-api.ini'
@@ -36,23 +35,28 @@ search_query_limit = 10
 # test comment
 comment = ' I’m sure you’ve heard it but\n\nKendrick Lamar - Humble (Skrillex Remix)\n\nAnd these are bangers\n\nMontell2099 - Hunnid on the Drop (Mr. Carmack Remix)\n\nASAP Rocky - Lord Pretty Flacko Joyde 2 (Y2K Remix)\n\nEkali - Babylon (feat. Denzel Curry) (Woodpile flip)\n\nBrockhampton - Stains (Whethan Bootleg)\n\nKrane and B. Lewis - PCP (feat. Nick Row)\n\nSorry bout no links I’m on mobile\n'
 
+# Open stored search results.
 with open('search_results-all.p', 'rb') as fp:
     all_search_results = pickle.load(fp)
+#  all_search_results = []
 
 # Regex used to determine if line is a song name. Matches are assumed to be
 # the artist and song name, order being irrelevant.
 song_name_regex = '((.+)\s-\s(.+))'
-#  all_search_results = []
+
+# Link to search of song when it can't be found.
+soundcloud_search_url = 'https://soundcloud.com/search?q='
+not_found_text = ' could not be found.'
 #  }}} global vars #
 
-#  authenticate() {{{ # 
-def authenticate():
+#  return_reddit_service() {{{ #
+def return_reddit_service():
     print('Authenticating...\n')
-    reddit = praw.Reddit('trapbot')
-    print(reddit.user.me())
-    print('Authenticated as {}\n'.format(reddit.user.me()))
-    return reddit
-#  }}} authenticate() #
+    reddit_service = praw.Reddit('trapbot')
+    print(reddit_service.user.me())
+    print('Authenticated as {}\n'.format(reddit_service.user.me()))
+    return reddit_service
+#  }}} return_reddit_service() #
 
 #  def return_cse_service(): {{{ # 
 def return_cse_service():
@@ -88,20 +92,22 @@ def compare_search_with_title(search_words, title_words):
         #  search_words_found, title_words, percentage_found * 100))
 #  }}} compare_search_with_title() # 
 
-#  get_best_song_url(google_cse_service, query): {{{ #
+#  get_best_song_url(cse_service, query): {{{ #
 # todo: remove last arg when actually using
-def get_best_song_url(google_cse_service, query, search_results_index):
+def get_best_song_url(cse_service, query, search_results_index):
     #  LIVE/store search code {{{ # 
     # LIVE search
-    #  search_results = google_cse_service.cse().list(q=query, cx=cse_id, num=search_query_limit).execute()
+    #  search_results = cse_service.cse().list(q=query, cx=cse_id, num=search_query_limit).execute()
     #  all_search_results.append(search_results)
     #  }}} LIVE/store search code # 
+    # print(query)
     search_results = all_search_results[search_results_index]
 
     most_similar_percent = 0
     best_url = ''
     for result in search_results['items']:
         url = result['link']
+        # print(url)
         if 'search' not in url:
             page_title = result['title'].encode("utf-8")
             song_name = result['pagemap']['metatags'][0]['twitter:title']
@@ -113,42 +119,35 @@ def get_best_song_url(google_cse_service, query, search_results_index):
                 if similar_percent > (most_similar_percent + 0.1):
                     most_similar_percent = similar_percent
                     best_url = url
+                    #  print(most_similar_percent, best_url)
 
-    return best_url
+    return best_url if most_similar_percent > .7 else None
 #  }}} get_best_song_url(api_key, query): # 
+
+#  def get_not_found_pair(song_name): {{{ # 
+def get_not_found_pair(song_name):
+    search_url = soundcloud_search_url + song_name.replace(" ", "%20").replace(")", "\)")
+    return [song_name, search_url, not_found_text]
+#  }}} def get_not_found_pair(song_name): # 
 
 #  def get_song_url_pairs(song_names_matches): {{{ #
 def get_song_url_pairs(song_names_matches):
     song_url_pairs = []
-    google_cse_service = return_cse_service()
+    cse_service = return_cse_service()
 
     # todo: remove when actually using
     results_index = 0
     for match in song_names_matches:
         # combine song name and artist to form search query
         search_query = "{0} {1}".format(match[1], match[2])
-        song_url_pairs.append([match[0], get_best_song_url(google_cse_service, search_query, results_index)])
+        best_song_url = get_best_song_url(cse_service, search_query, results_index)
+        if best_song_url is not None:
+            song_url_pairs.append([match[0], best_song_url])
+        else:
+            song_url_pairs.append(get_not_found_pair(match[0]))
         results_index += 1
 
     return song_url_pairs
-
-        #  SoundCloud API code {{{ # 
-        # using url file to test code so don't have to call SoundCloud API
-        #  url_file = open('urls.txt')
-        #  title_words_lst = []
-        #  for line in url_file:
-            #  last_slash_index = line.rfind('/')
-            #  if last_slash_index != -1:
-                #  title_words = line[last_slash_index + 1:].replace("\n", "").split('-')
-                #  title_words_lst.append(title_words)
-        # call SoundCloud API to retrieve tracks based on search_query
-        #  tracks = client.get('/tracks', q=search_query, limit=track_query_limit)
-        #  for track in tracks:
-            #  print("Track {0} Possible URL: {1}\n".format(i, track.permalink_url))
-            #  url = track.permalink_url
-            #  last_slash_index = url.rfind('/')
-            #  title_words = url[last_slash_index + 1:].replace("\n", "").split('-')
-        #  }}} SoundCloud API code # 
 #  }}} get_song_url_pairs() #
 
 def test_comment():
@@ -157,23 +156,23 @@ def test_comment():
 
 #  def scan_comments(): {{{ # 
 def scan_comments():
-    reddit = authenticate()
+    reddit_service = return_reddit_service()
     print("Getting {0} comments...\n".format(comment_query_limit))
 
-    # retrieve comments from reddit
-    for comment in reddit.subreddit(subreddits).comments(limit=comment_query_limit):
+    # retrieve comments from reddit_service
+    for comment in reddit_service.subreddit(subreddits).comments(limit=comment_query_limit):
         song_names_matches = re.findall(song_name_regex, comment.body)
 
-        file_obj_r = open(path,'r')
-        if comment.id not in file_obj_r.read().splitlines():
-            file_obj_r.close()
+        read_comments_file = open(comments_path, 'r')
+        if comment.id not in read_comments_file.read().splitlines():
+            read_comments_file.close()
             print('Link is new. Posting links.\n')
             post = return_comment(get_song_url_pairs(song_names_matches))
             comment.reply(post)
 
-            file_obj_w = open(path,'a+')
-            file_obj_w.write(comment.id + '\n')
-            file_obj_w.close()
+            write_comments_file = open(comments_path, 'a+')
+            write_comments_file.write(comment.id + '\n')
+            write_comments_file.close()
         else:
             print('Already visited link.\n')
 
@@ -187,9 +186,13 @@ def scan_comments():
 # Expects list of lists containing song then URL in that specific order.
 def return_comment(song_url_pairs):
     comment = header
-    for pair in song_url_pairs:
-        #  print("[{0}]({1})".format(pair[0], pair[1]))
-        comment += "[{0}]({1})\n\n".format(pair[0], pair[1])
+    for item in song_url_pairs:
+        #  print("[{0}]({1})".format(item[0], item[1]))
+        if len(item) == 3:
+            comment += "* ~~[{0}]({1})~~{2}".format(item[0], item[1], item[2])
+        else:
+            comment += "* [{0}]({1})".format(item[0], item[1])
+        comment += "\n\n"
     return comment + footer
 #  }}}  def return_comment(song_url_pairs): # 
 
